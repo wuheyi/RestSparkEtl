117c2a3d3e8180e8a6c9b2b4cc268bac70eb76cc
伍和义 <527153688@qq.com>
2018-05-06 11:57:23 +0800

    [init] spring boot 项目初始化

---
+++ src/src/main/java/hz/why/restsparketl/RestsparketlApplication.java
@@ -0,0 +1,12 @@
+package hz.why.restsparketl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestsparketlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestsparketlApplication.class, args);
+	}
+}
---
+++ src/src/test/java/hz/why/restsparketl/RestsparketlApplicationTests.java
@@ -0,0 +1,16 @@
+package hz.why.restsparketl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestsparketlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
