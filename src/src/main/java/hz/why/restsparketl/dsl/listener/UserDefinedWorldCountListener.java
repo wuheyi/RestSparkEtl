a9e5e850e7cb6aca10229d3a4b1a72fee187c76c
伍和义 <527153688@qq.com>
2018-05-23 00:26:18 +0800

    [update] 新增antlr的demo

--- src/src/main/java/hz/why/restsparketl/dsl/listener/UserDefinedWorldCountListener.java
+++ src/src/main/java/hz/why/restsparketl/dsl/listener/UserDefinedWorldCountListener.java
@@ -1,7 +1,72 @@
 package hz.why.restsparketl.dsl.listener;
 
+import hz.why.restsparketl.dsl.parser.WorldCountBaseListener;
+import hz.why.restsparketl.dsl.parser.WorldCountParser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
 /**
  * Created by wuheyi on 2018/5/23.
  */
-public class UserDefinedWorldCountListener {
+public class UserDefinedWorldCountListener extends WorldCountBaseListener {
+    @Override
+    public void enterProg(WorldCountParser.ProgContext ctx) {
+        super.enterProg(ctx);
+    }
+
+    @Override
+    public void exitProg(WorldCountParser.ProgContext ctx) {
+        super.exitProg(ctx);
+    }
+
+    @Override
+    public void enterStat(WorldCountParser.StatContext ctx) {
+        super.enterStat(ctx);
+    }
+
+    @Override
+    public void exitStat(WorldCountParser.StatContext ctx) {
+        super.exitStat(ctx);
+    }
+
+    @Override
+    public void enterExpr(WorldCountParser.ExprContext ctx) {
+        super.enterExpr(ctx);
+    }
+
+    @Override
+    public void exitExpr(WorldCountParser.ExprContext ctx) {
+        super.exitExpr(ctx);
+    }
+
+    @Override
+    public void enterFiledir(WorldCountParser.FiledirContext ctx) {
+        super.enterFiledir(ctx);
+    }
+
+    @Override
+    public void exitFiledir(WorldCountParser.FiledirContext ctx) {
+        super.exitFiledir(ctx);
+    }
+
+    @Override
+    public void enterEveryRule(ParserRuleContext ctx) {
+        super.enterEveryRule(ctx);
+    }
+
+    @Override
+    public void exitEveryRule(ParserRuleContext ctx) {
+        super.exitEveryRule(ctx);
+    }
+
+    @Override
+    public void visitTerminal(TerminalNode node) {
+        super.visitTerminal(node);
+    }
+
+    @Override
+    public void visitErrorNode(ErrorNode node) {
+        super.visitErrorNode(node);
+    }
 }
--- src/src/main/java/hz/why/restsparketl/dsl/walker/WorldCountWalker.java
+++ src/src/main/java/hz/why/restsparketl/dsl/walker/WorldCountWalker.java
@@ -1,7 +1,35 @@
 package hz.why.restsparketl.dsl.walker;
 
+import hz.why.restsparketl.dsl.listener.UserDefinedWorldCountListener;
+import hz.why.restsparketl.dsl.parser.WorldCountLexer;
+import hz.why.restsparketl.dsl.parser.WorldCountParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 /**
  * Created by wuheyi on 2018/5/23.
  */
 public class WorldCountWalker {
+    public static void walk(String input) {
+
+        // 对每一个输入的字符串，构造一个 ANTLRStringStream 流 in
+        ANTLRInputStream in = new ANTLRInputStream(input);
+
+        // 用 in 构造词法分析器 lexer，词法分析的作用是产生记号
+        WorldCountLexer lexer = new WorldCountLexer(in);
+
+        // 用词法分析器 lexer 构造一个记号流 tokens
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+
+        // 再使用 tokens 构造语法分析器 parser,至此已经完成词法分析和语法分析的准备工作
+        WorldCountParser parser = new WorldCountParser(tokenStream);
+
+        ParseTree tree = parser.prog();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new UserDefinedWorldCountListener(), tree);
+
+    }
 }
